0911b4fffe32b522eb690222213821aa46bb9afa
Nick P <nickpastoria@gmail.com>
2014-02-06 18:26:18 -0800

    Optimised some shit Fuck you
    yay fuck you

--- Visual Studio 2013/Projects/Better Calc/Better Calc/Calc_Main.cpp
+++ Visual Studio 2013/Projects/Better Calc/Better Calc/Calc_Main.cpp
@@ -17,50 +17,59 @@ double mult(double num1, double num2){
 double div(double num1, double num2){
 	return num1 / num2;
 }
-int check(){
+/*int check(int va){
 	int value(0);
-	while (!(cin >> value)) {
+	while (!(va >> value)) {
 		std::cout << "you entered a non-digit character\n";
 		std::cin.clear();   // make sure that std::cin is happy to do something
 		std::cin.ignore();  // ignore the offending character
 	}
-}
+	return 1;
+}*/
+/*int read(){
+	int c;
+	int a;
+	do {
+		c = getchar();
+		putchar(c);
+	} while (!(a >> 0));
+	return c;
+}*/
 int main(){
-	double num1, num2, answer;
+//	double num1, num2, answer;
+	int con(0);
 	aop = "#";
 	anum = 0;
 	anum1 = 0;
-	answer = 0;
+	double answer(0);
 	string op;
-	system("CLS");
-	cout << anum << " " << aop << " " << anum1 << "=" << answer << endl;
-
-	cin >> num1;
-	check();
-	anum = num1;
-	system("CLS");
-	cout << anum << " " << aop << " " << anum1 << "=" << answer << endl;
-
-	cin >> op;
-	aop = op;
-	system("CLS");
-	cout << anum << " " << aop << " " << anum1 << "=" << answer << endl;
-
-	cin >> num2;
-	check();
-	anum1 = num2;
-
-	if (op == "+"){
-		answer = add(num1, num2);
+	
+	while (con < 3){
+		system("CLS");
+		cout << anum << " " << aop << " " << anum1 << "=" << answer << endl;
+		if (con == 0){
+			cin >> anum;
+		}
+		if (con == 1){
+			cin >> aop;;
+		}
+		if (con == 2){
+			cin >> anum1;
+		}
+		con += 1;
 	}
-	if (op == "-"){
-		answer = sub(num1, num2);
+
+	if (aop == "+"){
+		answer = add(anum, anum1);
 	}
-	if (op == "*" || op == "x"){
-		answer = mult(num1, num2);
+	if (aop == "-"){
+		answer = sub(anum, anum1);
 	}
-	if (op == "/"){
-		answer = div(num1, num2);
+	if (aop == "*" || op == "x"){
+		answer = mult(anum, anum1);
+	}
+	if (aop == "/"){
+		answer = div(anum, anum1);
 	}
 	system("CLS");
 	cout << anum << " " << aop << " " << anum1 << "=" << answer << endl;
